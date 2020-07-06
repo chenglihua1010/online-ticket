1e8e98584b202ad58cb3a92ec24084de25bf14b2
wwclr <17710634541@163.com>
2020-07-06 21:40:07 +0800

    项目搭建

---
+++ online-ticket-service/src/main/java/com/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@ImportResource("classpath:spring-mybatis.xml")      //导入xml配置
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ConsumerApplication.class,args);
+    }
+}
