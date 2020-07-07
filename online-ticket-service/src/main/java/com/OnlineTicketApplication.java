b697027959fdc01d90882baef2ad55f81fe5a69b
wwclr <17710634541@163.com>
2020-07-07 23:31:21 +0800

    项目搭建

--- online-ticket-service/src/main/java/com/OnlineTicketApplication.java
+++ online-ticket-service/src/main/java/com/OnlineTicketApplication.java
@@ -2,9 +2,13 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @ImportResource("classpath:spring-mybatis.xml")      //导入xml配置
+@MapperScan(basePackages = "com.ticket.mapper")
+@ComponentScan(basePackages ={"com.ticket"} )
 @SpringBootApplication
 public class OnlineTicketApplication {
     public static void main(String[] args){
