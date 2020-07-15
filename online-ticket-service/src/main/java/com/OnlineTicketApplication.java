70e946fa7adaac7c15aea650f1d9a8d6f011c98f
wwclr <17710634541@163.com>
2020-07-15 22:57:56 +0800

    项目搭建

--- online-ticket-service/src/main/java/com/OnlineTicketApplication.java
+++ online-ticket-service/src/main/java/com/OnlineTicketApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import tk.mybatis.spring.annotation.MapperScan;
 
-//@ImportResource("classpath:spring-mybatis.xml,classpath:spring-redis.xml")      //导入xml配置
 @ImportResource(locations = {"classpath:spring-mybatis.xml","classpath:spring-redis.xml"})
 @MapperScan(basePackages = "com.ticket.mapper")
 @ComponentScan(basePackages ={"com.ticket.api"} )
