033d2e4d97731a65fa219345c3846b9127e4e8b1
wangwu <wangwu@ngarihealth.com>
2020-08-05 14:18:38 +0800

    增加切面捕获日志方法

--- online-ticket-service/src/main/java/com/OnlineTicketApplication.java
+++ online-ticket-service/src/main/java/com/OnlineTicketApplication.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@ImportResource(locations = {"classpath:spring-mybatis.xml","classpath:spring-redis.xml"})
+//@ImportResource(locations = {"classpath:spring-mybatis.xml","classpath:spring-redis.xml"})
+@ImportResource(locations = {"classpath:spring.xml"})
 @MapperScan(basePackages = "com.ticket.mapper")
 @ComponentScan(basePackages ={"com.ticket.api"} )
 @SpringBootApplication
